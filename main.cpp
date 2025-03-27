c515dc70852be7efd49384e884ae1ef790642d34
Jean Abreu <jean.abreu490@gmail.com>
2025-03-27 14:57:08 -0400

    Added a function called "showUsage", which will display the correct way to call the program. It will take the program name so that the usage message reflects how the program was invoked. Then, it will output two lines: one showing how to use the zip and one for unzip. Also included a conditional statement in the main. "argc" represents the number of command-line arguments. "argv" is an array of C-style strings representing each argument. The program expects exactly 4 arguments; -The program name -The mode (zip or unzip) -The input file name -The output file name If the count is not 4, the "showUsage" function is called to print usage instructions and returns a 1.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,16 @@
 #include <string>
 
 void showUsage(const std::string& programName){
-
+    std::cout << "Usage:\n"
+              << "  " << programName << " zip <input_file> <output_file>\n"
+              << "  " << programName << " unzip <input_file> <output_file>\n";
 }
 
 int main(int argc, char* argv[]){
-
+    if (argc != 4){
+        showUsage(argv[0]);
+        return 1;
+    }
 
     return 0;
 }
