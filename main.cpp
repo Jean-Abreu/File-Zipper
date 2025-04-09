23a85d203e979fa679a9e1f0679f42d09fbd7c30
Jean Abreu <jean.abreu490@gmail.com>
2025-04-09 13:18:25 -0400

    Made a conceptual format for this project. Currently, the code verifies file access and shows the correct mode. In later steps, we will need to incorporate real compression logic. Next steps will be to go over everything and comment what is occurring at each level, along with running a test to see if working as desired.

--- main.cpp
+++ main.cpp
@@ -14,5 +14,40 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
+    std::string mode = argv[1];
+    std::string inputFileName = argv[2];
+    std::string outputFileName = argv[3];
+
+    std::ifstream inputFile(inputFileName, std::ios::binary);
+    if(!inputFile){
+        std::cerr << "Error: Cannot open input file " << inputFileName << "'\n";
+        return 1;
+    }
+
+    std::ofstream outputFile(outputFileName, std::ios::binary);
+    if(!outputFile){
+        std::cerr << "Error: Cannot open output file '" << outputFileName << "'\n";
+        return 1;
+    }
+
+    if(mode == "zip") {
+        std::cout << "Zipping file: " << inputFileName << "\n";
+        //TODO: Implement compression
+    }
+
+    else if(mode == "unzip") {
+        std::cout << "Unzipping file: " << inputFileName << "\n";
+        //TODO: Implement decompression
+    }
+
+    else {
+        std::cerr << "Error: Unknown mode '" << mode << "'\n";
+        showUsage(argv[0]);
+        return 1;
+    }
+
+    inputFile.close();
+    outputFile.close();
+
     return 0;
 }
