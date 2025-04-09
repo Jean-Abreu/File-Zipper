24ab5ea194b4cee4f19a1c6e5ae69a5b82c510c0
Jean Abreu <jean.abreu490@gmail.com>
2025-04-09 13:55:35 -0400

    Commented on the logic of the code, will need to get familiar with everything. Ran a couple of test runs and on the base level everything is working well. There are a couple of files in the directory that are proof that the tests worked well. Need to familiarize myself with the console commands necesarry to make this work.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,38 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include <iostream> //Provides the input and output stream objects, allowing us to print messages to the console
+#include <fstream> //Offers file stream classses (ifstream/ofstream), this is important to be able to handle file I/O
+#include <string> //Will need this for manipulation of text data
 
-void showUsage(const std::string& programName){
+//This function is going to help display the controls for the program
+void showUsage(const std::string& programName){ //The parameter will be the program name (in this case argv[0])
     std::cout << "Usage:\n"
-              << "  " << programName << " zip <input_file> <output_file>\n"
-              << "  " << programName << " unzip <input_file> <output_file>\n";
-}
+              << "  " << programName << " zip <input_file> <output_file>\n" //Console will display how to use the zip mode
+              << "  " << programName << " unzip <input_file> <output_file>\n"; //Along with how to unzip
+} //Basically, this will show the user how to write commands for this program. 
 
-int main(int argc, char* argv[]){
-    if (argc != 4){
-        showUsage(argv[0]);
+int main(int argc, char* argv[]){ //argc = argument count || argv = array of strings holding the command-line arguments
+    if (argc != 4){ //Since our program expects 4 arguments, I have implemented a method that acts if the count is not four
+        showUsage(argv[0]); //If our condition is met, we show instructions and return an error
+        return 1;
+    }
+    //argv[0] is the program name
+    std::string mode = argv[1]; //Zip or unzip
+    std::string inputFileName = argv[2]; //File to read from
+    std::string outputFileName = argv[3]; //File to write results to
+
+    std::ifstream inputFile(inputFileName, std::ios::binary); //This line will open the input file in binary mode, ensuring the file is read exactly as is
+    if(!inputFile){ //Error handling in case file cannot be opened
+        std::cerr << "Error: Cannot open input file " << inputFileName << "'\n"; 
         return 1;
     }
 
-    std::string mode = argv[1];
-    std::string inputFileName = argv[2];
-    std::string outputFileName = argv[3];
-
-    std::ifstream inputFile(inputFileName, std::ios::binary);
-    if(!inputFile){
-        std::cerr << "Error: Cannot open input file " << inputFileName << "'\n";
-        return 1;
-    }
-
-    std::ofstream outputFile(outputFileName, std::ios::binary);
+    std::ofstream outputFile(outputFileName, std::ios::binary); //Same as before, except we are working with the output file
     if(!outputFile){
         std::cerr << "Error: Cannot open output file '" << outputFileName << "'\n";
         return 1;
     }
 
-    if(mode == "zip") {
-        std::cout << "Zipping file: " << inputFileName << "\n";
+    if(mode == "zip") { //Simple string comparisons to check the mode
+        std::cout << "Zipping file: " << inputFileName << "\n"; //Outputting a message to indicate which operation is being performed
         //TODO: Implement compression
     }
 
@@ -41,12 +42,12 @@ int main(int argc, char* argv[]){
     }
 
     else {
-        std::cerr << "Error: Unknown mode '" << mode << "'\n";
+        std::cerr << "Error: Unknown mode '" << mode << "'\n"; //Error handling in case mode does not match available modes
         showUsage(argv[0]);
         return 1;
     }
 
-    inputFile.close();
+    inputFile.close(); //Closing files that were opened in this process
     outputFile.close();
 
     return 0;
