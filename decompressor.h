df5f6caf349353f4d82e1117d3679ff908e9556a
Jean Abreu <jean.abreu490@gmail.com>
2025-04-14 13:23:06 -0400

    Created structure for decompressor.h

---
+++ decompressor.h
@@ -0,0 +1,15 @@
+#ifndef DECOMPRESSOR_H
+#define DECOMPRESSOR_H
+
+#include <string>
+#include <unordered_map>
+#include <memory>
+
+class Decompressor{
+    public:
+
+    private:
+
+};
+
+#endif //DECOMPRESSOR_H
